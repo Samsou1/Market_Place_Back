7d4f40dfb8739e3ea6a602873f04ee492e668049
Samsou1 <sami.linard.pro@gmail.com>
2022-12-01 11:46:03 +0100

    hotfix to log in log out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,19 +4,14 @@ class Users::SessionsController < Devise::SessionsController
     private
   
     def respond_with(resource, _opts = {})
-      if current_user
         render json: { message: 'You are logged in.', user: current_user }, status: :ok
-      else
-        render json: { message: 'You are not logged in.'}, status: :unauthorized
-      end
+
     end
   
     def respond_to_on_destroy
-      if !current_user
-        log_out_success
-      else
-        log_out_failure
-      end
+      log_out_success && return if current_user
+
+      log_out_failure
     end
   
     def log_out_success
